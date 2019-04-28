8c55d3aa623ca92880074def0f66e6a0d3a3e8f7
Jase Chen <xxmm@live.cn>
2019-04-28 22:24:38 +0800

    Update install_seafile.sh

--- install_seafile.sh
+++ install_seafile.sh
@@ -30,7 +30,7 @@ function install_sea() {
 	mv seafile-server*.tar.gz ./installed
 	mv seafile-server-6* seafile-server
 	#安装依赖环境
-	yum -y install python-setuptools python-imaging python-ldap MySQL-python python-memcached python-urllib3
+	apt -y install python-setuptools python-imaging python-ldap MySQL-python python-memcached python-urllib3
 	#进行安装
 	cd seafile-server && ./setup-seafile.sh
 
@@ -50,48 +50,15 @@ function install_sea() {
 	echo "阿里云用户请注意放行端口(8000/8082)：https://www.xiaoz.me/archives/9310"
 	echo "------------------------------------------------------"
 }
-# 暂时分开
-function install_sea_mysql_ubuntu16() {
-	cd /home/MyCloud
-	#下载安装包6.2.3 64bit
-	wget http://seafile-downloads.oss-cn-shanghai.aliyuncs.com/seafile-server_6.2.3_x86-64.tar.gz
-	#解压
-	tar -zxvf seafile-server_6.2.3_x86-64.tar.gz
-	mkdir installed
-	mv seafile-server*.tar.gz ./installed
-	mv seafile-server-6* seafile-server
-	#安装依赖环境
-    apt -y install mysql-server
-	apt -y install python2.7 libpython2.7 python-setuptools python-imaging python-ldap python-urllib3 ffmpeg python-pip python-mysqldb python-memcache
-pip install pillow moviepy
-	#进行安装
-	cd seafile-server && ./setup-seafile-mysql.sh
 
-	#启动服务
-	./seafile.sh start &&  ./seahub.sh start
-	#防火墙放行端口
-	chk_firewall
-	#开机启动
-	echo "/home/MyCloud/seafile-server/seafile.sh start" >> /etc/rc.d/rc.local
-	echo "/home/MyCloud/seafile-server/seahub.sh start" >> /etc/rc.d/rc.local
-	chmod u+x /etc/rc.d/rc.local
-	#获取IP
-	osip=$(curl https://api.ip.sb/ip)
-	echo "------------------------------------------------------"
-	echo "恭喜，安装完成。请访问：http://${osip}:8000"
-	echo "帮助文档请访问：https://www.xiaoz.me/archives/8480"
-	echo "阿里云用户请注意放行端口(8000/8082)：https://www.xiaoz.me/archives/9310"
-	echo "------------------------------------------------------"
-}
 
 echo "##########	欢迎使用Seafile一键安装脚本^_^	##########"
 
-echo "1.安装Seafile(SQLite, Centos7)"
-echo "2.安装Seafile(MySQL, Ubuntu16.04)"
-echo "3.卸载Seafile"
-echo "4.退出"
+echo "1.安装Seafile(SQLite, ubuntu18.04)"
+echo "2.卸载Seafile"
+echo "3.退出"
 declare -i stype
-read -p "请输入选项:（1.2.3.4）:" stype
+read -p "请输入选项:（1.2.3）:" stype
 
 if [ "$stype" == 1 ]
 	then
@@ -107,18 +74,6 @@ if [ "$stype" == 1 ]
 			install_sea
 		fi
 	elif [ "$stype" == 2 ]
-        then
-            if [ -e "/home/MyCloud" ]
-                then
-                echo "目录存在，请检查是否已经安装。"
-                exit
-            else
-                echo "目录不存在，创建目录..."
-                mkdir -p /home/MyCloud
-                #执行安装函数
-                install_sea
-            fi
-	elif [ "$stype" == 3 ]
 		then
 			/home/MyCloud/seafile-server/seafile.sh stop
 			/home/MyCloud/seafile-server/seahub.sh stop
@@ -126,7 +81,7 @@ if [ "$stype" == 1 ]
 			rm -rf /tmp/seahub_cache/*
 			echo '卸载完成.'
 			exit
-	elif [ "$stype" == 4 ]
+	elif [ "$stype" == 3 ]
 		then
 			exit
 	else
